a86733f6f74052194e221c611d4a7e28ab5f6d54
CardinalJV <jessy.viranaiken@icloud.com>
2024-12-30 19:01:52 +0100

    Update: add new components PopularMovies()

--- Sources/Views/LandingView.swift
+++ Sources/Views/LandingView.swift
@@ -11,7 +11,7 @@ import SwiftData
 struct ContentView: View {
   
   @Environment(MoviesController.self) private var moviesController
-  @State private var posters: [URL] = []
+  
   @State private var index: Int = 0
   
   var body: some View {
@@ -19,113 +19,114 @@ struct ContentView: View {
       ZStack{
         Color(red: 40/250, green: 40/250, blue: 40/250)
           .ignoresSafeArea(.all)
-        VStack{
-          if self.posters.isEmpty{
-            Text("Chargement en cours")
-          } else {
-            ImageLoader(imageUrl: self.posters[self.index])
-              .scaledToFit()
-              .opacity(0.25)
-              .overlay(alignment: .center){
-                VStack(spacing: 10){
-                  /* Text */
-                  VStack(alignment: .leading){
-                    HStack{
-                      Rectangle()
-                        .frame(width: 50, height: 4)
-                        .foregroundStyle(LinearGradient(colors: [.red, .white], startPoint: .leading, endPoint: .trailing))
-                      Text("Plus jamais a court d'idées")
-                        .foregroundStyle(.red)
+        ScrollView{
+          VStack(spacing: 30){
+            /* Posters */
+            if moviesController.posters.isEmpty{
+              Text("Chargement en cours")
+            } else {
+              ImageLoader(imageUrl: moviesController.posters[self.index])
+                .scaledToFit()
+                .opacity(0.25)
+                .overlay(alignment: .center){
+                  VStack(spacing: 10){
+                    /* Text */
+                    VStack(alignment: .leading){
+                      HStack{
+                        Rectangle()
+                          .frame(width: 50, height: 4)
+                          .foregroundStyle(LinearGradient(colors: [.red, .white], startPoint: .leading, endPoint: .trailing))
+                        Text("Plus jamais a court d'idées")
+                          .foregroundStyle(.red)
+                          .bold()
+                      }
+                      Text("Le film parfait en quelques clics")
+                        .font(.title)
                         .bold()
+                      Text("Action, comédie, drame ou science-fiction ? Découvrez des films qui correspondent à vos envies du moment.")
+                        .font(.title3)
                     }
-                    Text("Le film parfait en quelques clics")
-                      .font(.title)
-                      .bold()
-                    Text("Action, comédie, drame ou science-fiction ? Découvrez des films qui correspondent à vos envies du moment.")
-                      .font(.title3)
-                  }
-                  /* - */
-                  /* Button */
-                  VStack{
-                    Button {
-                      
-                    } label: {
-                      HStack{
-                        Text("Trouver mon film")
-                        Image(systemName: "magnifyingglass")
+                    /* - */
+                    /* Button */
+                    VStack{
+                      Button {
+                        
+                      } label: {
+                        HStack{
+                          Text("Trouver mon film")
+                          Image(systemName: "magnifyingglass")
+                        }
+                        .frame(width: 350, height: 50)
+                        .background(Color(red: 40/250, green: 40/250, blue: 40/250).opacity(0.75), in: RoundedRectangle(cornerRadius: 5))
                       }
-                      .frame(width: 350, height: 50)
-                      .background(Color(red: 40/250, green: 40/250, blue: 40/250).opacity(0.75))
-                      .clipShape(RoundedRectangle(cornerRadius: 5))
-                    }
-                    Button {
-                      
-                    } label: {
-                      HStack{
-                        Text("Film les mieux notés")
-                        Image(systemName: "star")
+                      Button {
+                        
+                      } label: {
+                        HStack{
+                          Text("Film les mieux notés")
+                          Image(systemName: "star")
+                        }
+                        .frame(width: 350, height: 50)
+                        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 5))
                       }
-                      .frame(width: 350, height: 50)
-                      .background(.ultraThinMaterial.opacity(0.25))
-                      .clipShape(RoundedRectangle(cornerRadius: 5))
                     }
+                    /* - */
                   }
-                  /* - */
+                  .padding()
+                  .foregroundStyle(.white)
                 }
-                .padding()
-                .foregroundStyle(.white)
-              }
+            }
+            /* - */
+            /* Popular */
+            PopularMovies()
+            /* - */
+            Spacer()
           }
-          Spacer()
         }
-      }
-      /* Toolbar */
-      .toolbar{
-        ToolbarItem(placement: .topBarLeading) {
-          HStack{
-            Image(systemName: "popcorn")
-              .font(.title3)
-              .foregroundStyle(.red)
+        /* Toolbar */
+        .toolbar{
+          ToolbarItem(placement: .topBarLeading) {
             HStack(spacing: 0){
-              Text("Mov")
-                .foregroundStyle(.white)
+              HStack{
+                Image(systemName: "popcorn")
+                  .font(.title3)
+                  .foregroundStyle(.red)
+//                  .symbolEffect(.pulse)
+                Text("Mov")
+                  .foregroundStyle(.red)
+              }
+              .shadow(color: .red, radius: 10)
               Text("ix")
-                .foregroundStyle(.red)
-                .shadow(color: .red, radius: 10)
+                .foregroundStyle(.white)
             }
             .font(.title)
             .bold()
           }
-        }
-        ToolbarItem(placement: .topBarTrailing) {
-          Button {
-            
-          } label: {
-            Image(systemName: "heart")
-              .font(.title3)
-              .foregroundStyle(.red)
-              .bold()
-          }
-
-        }
-      }
-      /* - */
-      .onAppear{
-        Timer.scheduledTimer(withTimeInterval: 4.0, repeats: true) { _ in
-          if self.index < self.posters.count - 1 {
-            self.index += 1
-          } else {
-            self.index = 0
+          ToolbarItem(placement: .topBarTrailing) {
+            Button {
+              
+            } label: {
+              Image(systemName: "heart")
+                .font(.title3)
+                .foregroundStyle(.red)
+                .bold()
+            }
           }
         }
-      }
-      .task {
-        await moviesController.fetchMoviesListsItems(page: 1)
-        for item in moviesController.moviesListsItems {
-          if let posterPath = item.posterPath {
-            self.posters.append(posterPath)
+        /* - */
+        .onAppear{
+          Timer.scheduledTimer(withTimeInterval: 4.0, repeats: true) { _ in
+            if self.index < moviesController.posters.count - 1 {
+              self.index += 1
+            } else {
+              self.index = 0
+            }
           }
-        }
+        } // Dynamique carroussel background
+        .task {
+          await moviesController.fetchMoviesListsItems(page: 1)
+          moviesController.getPostersForLandingView()
+        } // Add posters path in an array for more optimisations
       }
     }
   }
@@ -136,6 +137,6 @@ struct ContentView: View {
   @Previewable var moviesController = MoviesController()
   
   ContentView()
-  .modelContainer(for: Item.self, inMemory: true)
-  .environment(moviesController)
+    .modelContainer(for: Item.self, inMemory: true)
+    .environment(moviesController)
 }
