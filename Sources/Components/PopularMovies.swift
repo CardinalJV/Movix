73a274c1c67b3f8c5c8baecab60a7070e46ad692
CardinalJV <jessy.viranaiken@icloud.com>
2025-01-01 18:44:26 +0100

    Update: UI/UX and adda fonction in button

--- Sources/Components/PopularMovies.swift
+++ Sources/Components/PopularMovies.swift
@@ -1,9 +1,9 @@
-//
-//  PopularMovies.swift
-//  Movix
-//
-//  Created by Jessy Viranaiken on 30/12/2024.
-//
+  //
+  //  PopularMovies.swift
+  //  Movix
+  //
+  //  Created by Jessy Viranaiken on 30/12/2024.
+  //
 
 import SwiftUI
 import TMDb
@@ -12,44 +12,76 @@ struct PopularMovies: View {
   
   @Environment(MoviesController.self) private var moviesController
   
-  @State var movies: [MovieListItem] = []
+  @Namespace private var namespace
   
-    var body: some View {
-      VStack{
-        VStack(alignment: .leading, spacing: 0){
-          Rectangle()
-            .frame(width: 50, height: 4)
-            .foregroundStyle(LinearGradient(colors: [.red, .white], startPoint: .leading, endPoint: .trailing))
-          Text("Popular")
-            .font(.largeTitle)
-            .foregroundStyle(.white)
-            .bold()
-        }
-        .frame(width: 350, alignment: .leading)
-        if self.movies.isEmpty {
-          Text("No movie found on the database")
-        } else {
-          ScrollView{
-            LazyVGrid(columns: [GridItem(.flexible(), spacing: 15), GridItem(.flexible(), spacing: 15)], spacing: 15){
-              ForEach(self.movies) { movie in
-                VStack{
-                  ImageLoader(imageUrl: movie.posterPath)
-                    .scaledToFit()
-                    .frame(height: 250)
-                    .clipShape(RoundedRectangle(cornerRadius: 5))
-                }
+  @State private var showAllMovies = false
+  @State private var movies: [MovieListItem] = []
+  
+  var body: some View {
+    VStack(alignment: .center, spacing: 15){
+      /* Title */
+      VStack(alignment: .leading, spacing: 0){
+        Rectangle()
+          .frame(width: 50, height: 4)
+          .foregroundStyle(LinearGradient(colors: [.red, .white], startPoint: .leading, endPoint: .trailing))
+        Text("Populaires")
+          .font(.largeTitle)
+          .foregroundStyle(.white)
+          .bold()
+      }
+      .frame(width: 350, alignment: .leading)
+      /* - */
+      if self.movies.isEmpty {
+        Text("No movie found on the database")
+      } else {
+        /* Grid */
+        LazyVGrid(columns: [GridItem(.flexible(), spacing: 15), GridItem(.flexible(), spacing: 15)], spacing: 15){
+          ForEach(self.movies.prefix(self.showAllMovies ? self.movies.count : 10)) { movie in
+            NavigationLink {
+              MovieView(id: movie.id)
+                .navigationTransition(.zoom(sourceID: "zoom", in: self.namespace))
+            } label: {
+              VStack{
+                ImageLoader(imageUrl: movie.posterPath)
+                  .scaledToFit()
+                  .frame(height: 250)
+                  .clipShape(RoundedRectangle(cornerRadius: 5))
               }
+              .transition(.slide)
             }
+
           }
         }
-      }
-      .frame(width: 350)
-      .task {
-        if let movies = await moviesController.fetchPopularMovies() {
-          self.movies = movies
+        Button {
+          self.showAllMovies.toggle()
+        } label: {
+          HStack{
+            Image(systemName: (self.showAllMovies ? "arrow.up.square.fill" : "arrow.down.square.fill"))
+              .tint(.white)
+              .bold()
+              .animation(.bouncy, value: self.showAllMovies)
+            Text(self.showAllMovies ? "Voir moins" : "Voir plus")
+              .bold()
+              .foregroundStyle(.white)
+            Image(systemName: (self.showAllMovies ? "arrow.up.square.fill" : "arrow.down.square.fill"))
+              .tint(.white)
+              .bold()
+              .animation(.bouncy, value: self.showAllMovies)
+            
+          }
+          .frame(width: 350, height: 50)
+          .background(.red, in: RoundedRectangle(cornerRadius: 5))
         }
+        /* - */
       }
     }
+    .frame(width: 350)
+    .task {
+      if let movies = await moviesController.fetchPopularMovies() {
+        self.movies = movies
+      }
+    }
+  }
 }
 
 #Preview {
---
+++ Sources/Views/MovieView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieView.swift
+//  Movix
+//
+//  Created by Jessy Viranaiken on 01/01/2025.
+//
+
+import SwiftUI
+
+struct MovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieView()
+}
