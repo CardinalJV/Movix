41d2321333431eea97bb7867a338cac4d7de6ff8
CardinalJV <jessy.viranaiken@icloud.com>
2025-01-01 18:46:21 +0100

    Create: Add a new view for movie details

--- Sources/Views/MovieView.swift
+++ Sources/Views/MovieView.swift
@@ -1,18 +1,79 @@
-//
-//  MovieView.swift
-//  Movix
-//
-//  Created by Jessy Viranaiken on 01/01/2025.
-//
+  //
+  //  MovieView.swift
+  //  Movix
+  //
+  //  Created by Jessy Viranaiken on 01/01/2025.
+  //
 
 import SwiftUI
+import TMDb
 
 struct MovieView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  
+  @Environment(MoviesController.self) private var moviesController
+  @Environment(\.dismiss) private var dismiss
+  
+  @State private var movie: Movie? = nil
+  
+  let id: Int
+  
+  var body: some View {
+    VStack{
+        if let movie = self.movie {
+          ImageLoader(imageUrl: movie.backdropPath)
+            .scaledToFit()
+            .overlay{
+              HStack{
+                Button {
+                  dismiss()
+                } label: {
+                    Image(systemName: "xmark")
+                      .padding(8)
+                      .font(.title3)
+                      .bold()
+                      .foregroundStyle(.white)
+                      .background(Color(red: 40/250, green: 40/250, blue: 40/250), in: Circle())
+                }
+                Spacer()
+              }
+              .padding()
+              .frame(width: 425, height: 226, alignment: .top)
+              .background(Color.black.opacity(0.5))
+            }
+          VStack{
+            ImageLoader(imageUrl: movie.posterPath)
+              .scaledToFit()
+              .frame(width: 200)
+              .clipShape(RoundedRectangle(cornerRadius: 5))
+              .zIndex(1)
+            Text(movie.title)
+              .font(.title)
+              .bold()
+              .foregroundStyle(.white)
+          }
+          .frame(width: 350)
+          .offset(y: -125)
+          Spacer()
+        }
     }
+    .background(content: {
+      Color(red: 40/250, green: 40/250, blue: 40/250)
+        .ignoresSafeArea(.all)
+    })
+    .navigationBarBackButtonHidden(true)
+    .task {
+      if let movie = await moviesController.fetchMovie(byId: self.id) {
+        self.movie = movie
+      }
+    }
+  }
 }
 
 #Preview {
-    MovieView()
+  
+  @Previewable var moviesController = MoviesController()
+  
+  ContentView()
+    .modelContainer(for: Item.self, inMemory: true)
+    .environment(moviesController)
 }
