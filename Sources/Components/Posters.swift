0f757e0cfe5fd67d6853d0158e7f026f8daccb80
Viranaiken Jessy <viranaikenjessy@MacBook-Air-69.local>
2025-10-21 22:52:10 +0200

    Update: UI/UX

--- Sources/Components/Posters.swift
+++ Sources/Components/Posters.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct Posters: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    let posterPath: URL
+    let backdropPath: URL
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader{ geo in
+                ZStack{
+                    /* BackPoster */
+                    ImageLoader(imageUrl: backdropPath)
+//                        .frame(width: geo.size.width, height: geo.size.height)
+                        .opacity(0.75)
+                        .border(.green)
+                        .overlay(alignment: .topLeading){
+                            VStack{
+                                Button {
+                                    dismiss()
+                                } label: {
+                                    Image(systemName: "xmark")
+                                        .padding(8)
+                                        .font(.title3)
+                                        .bold()
+                                        .foregroundStyle(.white)
+                                        .background(Color(red: 40/250, green: 40/250, blue: 40/250), in: Circle())
+                                }
+                                Spacer()
+                            }
+                            .padding(10)
+                        }
+                    /* - */
+                    /* Main poster and title */
+                    ImageLoader(imageUrl: posterPath)
+                        .border(.blue)
+                        .frame(width: geo.size.width * 0.5, height: geo.size.height * 0.25)
+                        .position(x: geo.size.width * 0.5, y: geo.size.height * 25)
+                        .shadow(radius: 10)
+                    /* - */
+                    Color.clear
+                        .frame(width: geo.size.width, height: geo.size.height)
+                }
+                .border(.red)
+        }
+        .border(.yellow)
     }
 }
 
-#Preview {
-    Posters()
-}
+//#Preview {
+//
+//}
