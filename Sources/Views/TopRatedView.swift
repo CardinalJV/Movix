9bcd46b0c78fea2e0b3a44c593365ee268126cb7
Viranaiken Jessy <viranaikenjessy@MacBook-Air-69.local>
2025-10-21 22:55:29 +0200

    Create: Adding a new view for the top rated movies

--- Sources/Views/TopRatedView.swift
+++ Sources/Views/TopRatedView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct TopRatedView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LazyVGrid(columns: [GridItem(.flexible(), spacing: 15), GridItem(.flexible(), spacing: 15)], spacing: 15){
+          ForEach(self.movies.prefix(self.showAllMovies ? self.movies.count : 10)) { movie in
+            NavigationLink {
+              MovieView(id: movie.id)
+                .navigationTransition(.zoom(sourceID: "zoom", in: self.namespace))
+            } label: {
+              VStack{
+                ImageLoader(imageUrl: movie.posterPath)
+                  .scaledToFit()
+                  .frame(height: 250)
+                  .clipShape(RoundedRectangle(cornerRadius: 5))
+              }
+              .animation(.bouncy, value: self.showAllMovies)
+            }
+
+          }
+        }
     }
 }
 
