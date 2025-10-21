3e82e7ece4f1fd4a9c78f950c28d60148bb6a353
Viranaiken Jessy <viranaikenjessy@MacBook-Air-69.local>
2025-10-21 22:49:47 +0200

    Create: New components

--- Sources/Components/GlowingAnimation.swift
+++ Sources/Components/GlowingAnimation.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct GlowingAnimation: View {
+    
+    @State private var isAnimating: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .fill(AngularGradient(colors: [.red], center: .center, angle: .degrees(self.isAnimating ? 360 : 0)))
+                .frame(width: 75, height: 25)
+                .blur(radius: 40)
+//            RoundedRectangle(cornerRadius: 20)
+//                .fill(Color(red: 40/250, green: 40/250, blue: 40/250))
+//                .frame(width: 125, height: 50)
+            HStack(spacing: -2){
+                HStack{
+                    Image(systemName: "popcorn")
+                        .font(.title3)
+                    Text("Mov")
+                }
+                .foregroundStyle(.red)
+                Text("ix")
+                    .foregroundStyle(.white)
+            }
+            .font(.custom("PermanentMarker-Regular", size: 28))
+            .bold()
+        }
+        .onAppear{
+            withAnimation(Animation.linear(duration: .infinity).repeatForever(autoreverses: false)) {
+                self.isAnimating = true
+            }
+        }
+        .onDisappear{
+            self.isAnimating = false
+        }
     }
 }
 
