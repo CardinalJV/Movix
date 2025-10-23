5a41c419950ca302f758cb7e657952e84b40fba6
Viranaiken Jessy <viranaikenjessy@MacBook-Air-69.local>
2025-10-23 23:21:04 +0200

    Update: UI/UX

--- Sources/Views/TopRatedView.swift
+++ Sources/Views/TopRatedView.swift
@@ -7,28 +7,28 @@
 
 import SwiftUI
 
-struct TopRatedView: View {
-    var body: some View {
-        LazyVGrid(columns: [GridItem(.flexible(), spacing: 15), GridItem(.flexible(), spacing: 15)], spacing: 15){
-          ForEach(self.movies.prefix(self.showAllMovies ? self.movies.count : 10)) { movie in
-            NavigationLink {
-              MovieView(id: movie.id)
-                .navigationTransition(.zoom(sourceID: "zoom", in: self.namespace))
-            } label: {
-              VStack{
-                ImageLoader(imageUrl: movie.posterPath)
-                  .scaledToFit()
-                  .frame(height: 250)
-                  .clipShape(RoundedRectangle(cornerRadius: 5))
-              }
-              .animation(.bouncy, value: self.showAllMovies)
-            }
-
-          }
-        }
-    }
-}
-
-#Preview {
-    TopRatedView()
-}
+//struct TopRatedView: View {
+//    var body: some View {
+//        LazyVGrid(columns: [GridItem(.flexible(), spacing: 15), GridItem(.flexible(), spacing: 15)], spacing: 15){
+//          ForEach(self.movies.prefix(self.showAllMovies ? self.movies.count : 10)) { movie in
+//            NavigationLink {
+//              MovieView(id: movie.id)
+//                .navigationTransition(.zoom(sourceID: "zoom", in: self.namespace))
+//            } label: {
+//              VStack{
+//                ImageLoader(imageUrl: movie.posterPath)
+//                  .scaledToFit()
+//                  .frame(height: 250)
+//                  .clipShape(RoundedRectangle(cornerRadius: 5))
+//              }
+//              .animation(.bouncy, value: self.showAllMovies)
+//            }
+//
+//          }
+//        }
+//    }
+//}
+//
+//#Preview {
+//    TopRatedView()
+//}
