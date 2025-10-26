9bd6f3d4d3f21efbf1652871bf482df2121e53b7
CardinalJV <jessy.viranaiken@icloud.com>
2025-10-26 22:24:52 +0100

    Create: New TopRatedMovie

--- Sources/Views/TopRatedView.swift
+++ Sources/Views/TopRatedView.swift
@@ -7,28 +7,35 @@
 
 import SwiftUI
 
-//struct TopRatedView: View {
-//    var body: some View {
-//        LazyVGrid(columns: [GridItem(.flexible(), spacing: 15), GridItem(.flexible(), spacing: 15)], spacing: 15){
-//          ForEach(self.movies.prefix(self.showAllMovies ? self.movies.count : 10)) { movie in
-//            NavigationLink {
-//              MovieView(id: movie.id)
-//                .navigationTransition(.zoom(sourceID: "zoom", in: self.namespace))
-//            } label: {
-//              VStack{
-//                ImageLoader(imageUrl: movie.posterPath)
-//                  .scaledToFit()
-//                  .frame(height: 250)
-//                  .clipShape(RoundedRectangle(cornerRadius: 5))
-//              }
-//              .animation(.bouncy, value: self.showAllMovies)
-//            }
-//
-//          }
-//        }
-//    }
-//}
-//
+struct TopRatedView: View {
+    
+    
+    var body: some View {
+        ZStack{
+            Color(red: 40/250, green: 40/250, blue: 40/250)
+                .ignoresSafeArea(.all)
+            ScrollView{
+                /* Header */
+                VStack(alignment: .leading, spacing: 0){
+                    Rectangle()
+                        .frame(width: 50, height: 4)
+                        .foregroundStyle(LinearGradient(colors: [.red, .white], startPoint: .leading, endPoint: .trailing))
+                    Text("Populaires")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .bold()
+                }
+                .frame(width: 350, alignment: .leading)
+                /* - */
+                /* TopRatedMovies */
+                TopRatedMovies()
+                /* - */
+            }
+        }
+        .tint(.red)
+    }
+}
+
 //#Preview {
 //    TopRatedView()
 //}
