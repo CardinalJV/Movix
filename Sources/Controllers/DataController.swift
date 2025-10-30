ae4b5f866944f12fefe319bc63efba8ffc6d1052
CardinalJV <129071629+CardinalJV@users.noreply.github.com>
2025-10-30 23:22:48 +0100

    Update: Logics

--- Sources/Controllers/DataController.swift
+++ Sources/Controllers/DataController.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import SwiftData
+import TMDb
+
+@Observable
+class DataController{
+    
+    var dataItems: [DataItem] = []
+    var context: ModelContext?
+    var favoriteMovies: [Movie] = []
+    
+    func addInFavorites(movie: Movie){
+        self.favoriteMovies.append(movie)
+    }
+    
+    func deleteToFavorite(movieId: Int){
+        self.favoriteMovies.removeAll(where: { $0.id == movieId })
+    }
+    
+    func isInFavorites(movieId: Int) -> Bool {
+        if self.favoriteMovies.firstIndex(where: { $0.id == movieId }) != nil {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func fetchData() {
+      guard let context = self.context else {
+        return
+      }
+      let request = FetchDescriptor<DataItem>()
+      do {
+          self.dataItems = try context.fetch(request)
+      } catch {
+        print("Error during fetching data: \(error.localizedDescription)")
+      }
+    }
+}
