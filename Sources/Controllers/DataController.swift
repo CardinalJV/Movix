9fb36dc57e9e9a462bc11b5c0ec4f06122faba09
CardinalJV <129071629+CardinalJV@users.noreply.github.com>
2025-11-13 23:04:20 +0100

    Update: Optimisation

--- Sources/Controllers/DataController.swift
+++ Sources/Controllers/DataController.swift
@@ -10,37 +10,63 @@ import SwiftData
 import TMDb
 
 @Observable
-class DataController{
+class DataController {
     
     var dataItems: [DataItem] = []
-    var context: ModelContext?
     var favoriteMovies: [Movie] = []
+    var context: ModelContext?
     
-    func addInFavorites(movie: Movie){
-        self.favoriteMovies.append(movie)
-    }
-    
-    func deleteToFavorite(movieId: Int){
-        self.favoriteMovies.removeAll(where: { $0.id == movieId })
-    }
-    
-    func isInFavorites(movieId: Int) -> Bool {
-        if self.favoriteMovies.firstIndex(where: { $0.id == movieId }) != nil {
-            return true
-        } else {
-            return false
+    func addInFavorites(movie: Movie) {
+        guard let context = self.context else {
+            print("Context has a error")
+            return
+        }
+        if !self.favoriteMovies.contains(where: { $0.id == movie.id }) {
+            self.favoriteMovies.append(movie)
+        }
+        do {
+            context.insert(DataItem(tmdbID: movie.id))
+            try context.save()
+        } catch {
+            print("Error during adding in local storage: \(error.localizedDescription)")
         }
     }
     
+    func deleteToFavorite(movieId: Int) {
+        guard let context = self.context else {
+            print("Context has a error")
+            return
+        }
+        if self.favoriteMovies.contains(where: { $0.id == movieId }) {
+            self.favoriteMovies.removeAll(where: { $0.id == movieId})
+            self.dataItems.removeAll(where: { $0.tmdbID == movieId})
+        }
+        do {
+            for dataItem in dataItems {
+                if dataItem.tmdbID == movieId {
+                    context.delete(dataItem)
+                    try context.save()
+                }
+            }
+        } catch {
+            print("Error during deleting in local storage: \(error.localizedDescription)")
+        }
+    }
+    
+    
+    func isInFavorites(movieId: Int) -> Bool {
+        self.favoriteMovies.contains(where: { $0.id == movieId })
+    }
+    
     func fetchData() {
-      guard let context = self.context else {
-        return
-      }
-      let request = FetchDescriptor<DataItem>()
-      do {
-          self.dataItems = try context.fetch(request)
-      } catch {
-        print("Error during fetching data: \(error.localizedDescription)")
-      }
+        guard let context = self.context else {
+            return
+        }
+        let request = FetchDescriptor<DataItem>()
+        do {
+            self.dataItems = try context.fetch(request)
+        } catch {
+            print("Error during fetching data: \(error.localizedDescription)")
+        }
     }
 }
